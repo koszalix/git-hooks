ca43578edfb51ea31f1510dabcc359cab2f55aba
pawel koszalka <koszalix@gmail.com>
2023-07-22 23:32:03 +0200

    Fix typos

--- hook-creator.sh
+++ hook-creator.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
-
-if [[ ! -e $1 ]]
+if [[ -z $1 ]]
 then
         echo "hook-creator"
         echo "usage: hook-creator <hook-name> <hook-type>"
         echo "available hooks: "
-        echo "\t check_shell"
-        echo "available types"
-        echo "\t post-update"
-else
-        if [[ -d .git/hooks ]] 
+        echo "  check_shell"
+              
+else    
+        # check if there is a git hooks directory
+        if [[ ! -e .git/hooks ]] 
         then
                 echo "Couldn't find a hook directory are you in git repo directory?"
                 exit
         fi
+
+
+        # check if hook type is specified
+        if [[  -z $2 ]]
+        then
+                                
+                echo "hook type not specified, using default one"
+        fi
+
+        # set the check_shell
         if [[ $1 == "check_shell" ]]
         then
-                if [[ ! -e $2 ]]
+                if [[  -z $2 ]]
                 then
-
-                        curl https://raw.githubusercontent.com/koszalix/git-hooks/main/check_shell.sh \
-                        -o .git/hooks/post-commit
+                        TYPE="post-commit"
                 else
-                        curl https://raw.githubusercontent.com/koszalix/git-hooks/main/check_shell.sh
-                        -o .git/hooks/$2
-                fi
-
+                        TYPE=$2
+                fi      
+     
+                curl https://raw.githubusercontent.com/koszalix/git-hooks/main/check_shell.sh \
+                -o .git/hooks/"${TYPE}"
+                chmod +x .git/hooks/"${TYPE}"
+        
+        # unidentified hook
         else
                 echo "$1 hook doesn't exist"
                 exit
