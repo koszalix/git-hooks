98a16353c4b2f808d399918677f740d42ed15860
pawel koszalka <koszalix@gmail.com>
2023-07-22 19:34:51 +0200

    Add tee to shell check

--- check_shell.sh
+++ check_shell.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo "start of shell check"
-find . -not -type d -name '*.sh' -exec shellcheck --enable=all {} +  > shellcheck.txt
+echo -e "\033[0;1mStart of shell check"
+find . -not -type d -name '*.sh' -exec shellcheck --enable=all {} +\
+| tee  shellcheck.txt || true 
 
 len=$(wc -w shellcheck.txt | cut -c 1 || true) 
 
 
 if [[ ${len} == "0" ]]
 then
-	echo -e "\033[0;1m Shell check: \033[0;32m PASS! \033[0;0m"
+	echo -e "\033[0;1mShell check: \033[0;32m PASS! \033[0;0m"
 else
-	echo -e "\033[0;1m Shell check:  \033[0;31m FAIL! \033[0;0m"
+	echo -e "\033[0;1mShell check:  \033[0;31m FAIL! \033[0;0m"
 fi
 
